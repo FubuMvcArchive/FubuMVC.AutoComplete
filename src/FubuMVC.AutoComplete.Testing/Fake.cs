e756e07e800a940fcb1d48b3b151f07f50afe1a8
jeremydmiller <jeremydmiller@yahoo.com>
2012-12-19 20:13:38 -0600

    initial commit of code, skeleton/spike level code

---
+++ src/FubuMVC.AutoComplete.Testing/Fake.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using FubuTestingSupport;
+
+namespace FubuMVC.AutoComplete.Testing
+{
+    [TestFixture]
+    public class Fake
+    {
+        [Test]
+        public void NAME()
+        {
+            true.ShouldBeTrue();
+        }
+    }
+}
